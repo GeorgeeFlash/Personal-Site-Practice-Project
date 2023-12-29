07bc99a52f171e378135e5e482b6e9b592c9656e
GeorgeeFlash <georgeeflash@gmail.com>
2023-12-29 10:42:45 +0100

    removed colsole.logs

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -8,9 +8,6 @@ type Props = {
 export default async function Page({ params }: Props) {
     const page = await getPage(params.slug)
 
-    console.log("Slug: ", params.slug)
-    // console.log("Data: ", page)
-
     return (
         <div>
             <h1 className="bg-gradient-to-r from-red-500 via-blue-500 to-yellow-500 bg-clip-text text-transparent text-5xl font-extrabold">{page.title}</h1>
