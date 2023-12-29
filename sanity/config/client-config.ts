341dcaf5a83a922a6f63700e9042b3404e2d03eb
GeorgeeFlash <georgeeflash@gmail.com>
2023-12-29 12:03:49 +0100

    Created token for assets and ordered posts in descending order by creation

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,7 +2,9 @@ const config = {
     projectId: "gstbclx0",
     dataset: "production",
     apiVersion: "2023-12-28",
-    useCdn: false
+    useCdn: false,
+    token: process.env.NEXT_SANITY_TOKEN,
+    ignoreBrowserTokenWarning: true,
 }
 
 export default config;
